ea0a321d750a36cfbc6a030b3ef9b2c9d54ba57e
davidtypes <ddvalenzuela904@gmail.com>
2025-08-30 14:39:24 -0700

    Creat channel modal created, functionality implemented.

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
+import {CreateChannelModal} from "@/components/modals/create-channel-modal";
 
 import { useEffect, useState } from "react";
 
@@ -25,6 +26,7 @@ export const ModalProvider = () => {
             <InviteModal />
             <EditServerModal />
             <MembersModal />
+            <CreateChannelModal />
         </>
     )
 }
